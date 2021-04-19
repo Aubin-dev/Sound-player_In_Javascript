15eb76ca38414d32480e71c04423ef311f32fc86
mida5579 <bandamaaubin@gmail.com>
2021-04-19 16:53:15 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,98 @@
+
+
+// document.body.append('test');
+// let helloWorld= document.createElement('div');
+// helloWorld.textContent='hello world';
+
+// document.body.append(helloWorld)
+
+// let h1 = document.querySelector('h1');
+
+
+
+// // Création des éléments
+// let header = document.createElement('header');
+// let menu = document.createElement('div')
+// let paragraphe = document.createElement('p')
+
+
+// // Contenu et style du header
+// header.textContent='Bienvenue sur le Dom';
+
+// header.style.backgroundColor ='red'
+// header.style.fontSize ='3em';
+// header.style.color ='white';
+// header.style.padding ='35px';
+// header.style.textAlign='center';
+
+
+// // Contenu et style du menu
+// menu.innerHTML= "<a href='#'>Accueil</a> <a href='#'>Une autre page</a>"
+
+// menu.style.display="flex";
+// menu.style.justifyContent="space-around";
+// menu.style.textAlign="center";
+// menu.style.backgroundColor ='orange'
+// menu.style.padding='15px';
+
+
+// // Ajout des éléments au DOM
+// document.body.append(header,menu);
+
+// let heuresDiv = document.createElement('div')
+// document.body.style.display='flex'
+// document.body.style.textAlign='center'
+// document.body.style.alignContent='center'
+// document.body.style.justifyContent='center'
+// document.body.style.fontFamily='"Nunito",sans-serif'
+
+
+
+// let heureDiv = document.createElement('div');
+
+// function afficheHeure(){
+
+//     now =new Date();
+
+    
+// }
+// afficheHeure()
+
+// setInterval(afficheHeure,1000)
+
+
+//const musics =  'https://api.discogs.com/releases/249504';
+// fetch(musics)
+//     .then ( response  =>  response . json ( ) )  // accès aux données API en JSON 
+//     .then ( data  =>  {  console. log ( data ) }) 
+//     .catch ( erreur  =>  console . log ( erreur ) ) 
+
+
+
+
+function volumechange(){
+
+};
+function changeDuration(){
+
+};
+function playPrevious(){
+    
+};
+function justPlay(){
+    
+};
+function playNext(){
+
+};
+function muteSound(){
+    
+}
+
+
+
+
+
+
+
+
