1863598ab4296d08cc9fd09b7eb13ff7df06c1d1
Aubin-Dev (Bandama aubin) <78278284+Aubin-dev@users.noreply.github.com>
2021-04-20 12:57:27 +0000

    second modifs

--- app.js
+++ app.js
@@ -1,98 +1,63 @@
-
-
-// document.body.append('test');
-// let helloWorld= document.createElement('div');
-// helloWorld.textContent='hello world';
-
-// document.body.append(helloWorld)
-
-// let h1 = document.querySelector('h1');
-
-
-
-// // Création des éléments
-// let header = document.createElement('header');
-// let menu = document.createElement('div')
-// let paragraphe = document.createElement('p')
-
-
-// // Contenu et style du header
-// header.textContent='Bienvenue sur le Dom';
-
-// header.style.backgroundColor ='red'
-// header.style.fontSize ='3em';
-// header.style.color ='white';
-// header.style.padding ='35px';
-// header.style.textAlign='center';
-
-
-// // Contenu et style du menu
-// menu.innerHTML= "<a href='#'>Accueil</a> <a href='#'>Une autre page</a>"
-
-// menu.style.display="flex";
-// menu.style.justifyContent="space-around";
-// menu.style.textAlign="center";
-// menu.style.backgroundColor ='orange'
-// menu.style.padding='15px';
-
-
-// // Ajout des éléments au DOM
-// document.body.append(header,menu);
-
-// let heuresDiv = document.createElement('div')
-// document.body.style.display='flex'
-// document.body.style.textAlign='center'
-// document.body.style.alignContent='center'
-// document.body.style.justifyContent='center'
-// document.body.style.fontFamily='"Nunito",sans-serif'
+let previous = document.querySelector('#pre');
+let play = document.querySelector('#play');
+let next = document.querySelector('#next');
+let title = document.querySelector('#title');
+let recent_volume = document.querySelector('#volume');
+let volume_show = document.querySelector('#volume_show');
+let artist = document.querySelector('#artist');
+let total = document.querySelector('#total');
+let present = document.querySelector('#present');
+let slider = document.querySelector('#duration_slider');
+let show_duration = document.querySelector('#show_duration');
+let track_duration = document.querySelector('#track_duration');
+let auto_play = document.querySelector('#auto');
 
 
 
-// let heureDiv = document.createElement('div');
+let timer = 0;
+let autoplay = 0;
 
-// function afficheHeure(){
-
-//     now =new Date();
-
-    
-// }
-// afficheHeure()
-
-// setInterval(afficheHeure,1000)
+let index_no = 0;
+let playing_song = false;
 
 
-//const musics =  'https://api.discogs.com/releases/249504';
-// fetch(musics)
-//     .then ( response  =>  response . json ( ) )  // accès aux données API en JSON 
-//     .then ( data  =>  {  console. log ( data ) }) 
-//     .catch ( erreur  =>  console . log ( erreur ) ) 
+let track = document.createElement('audio')
+
+
+let All_song = [{
+    name: "fi"
+}]
 
 
 
 
-function volumechange(){
+function volumechange() {
 
 };
-function changeDuration(){
+
+function changeDuration() {
 
 };
-function playPrevious(){
-    
-};
-function justPlay(){
-    
-};
-function playNext(){
+
+function playPrevious() {
 
 };
-function muteSound(){
-    
+
+function justPlay() {
+
+};
+
+function playNext() {
+
+};
+
+function muteSound() {
+
 }
 
+const url = " https://musicbrainz.org/ws/2/"
 
-
-
-
-
-
-
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
